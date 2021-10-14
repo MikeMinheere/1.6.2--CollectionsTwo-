7b187daf135660e08bb29602cb076303b3c71be0
Mike Minheere <99067548@mydavinci.nl>
2021-10-14 09:48:08 +0200

    kaartenSet

---
+++ kaartenSet.py
@@ -0,0 +1,28 @@
+import random
+def kaartenDek():
+    dek = list()
+    for i in range(0,54):
+        randomChoice = random.randint(0,54)
+        dek.append(soorten[randomChoice])
+    return dek
+jokers = ['joker,', 'Joker,']
+dek = list()
+soorten = ['klaveren ', 'harten ', 'schoppen ', 'ruiten ']
+cijfer = ['1,','2,','3,','4,','5,','6,','7,','8,','9,','10,','boer,','vrouw,','heer,','aas,']
+for x in range (0,len(soorten)):
+    for y in range(0,len(cijfer)):
+        dek.append(soorten[x]+cijfer[y])
+dek.append(jokers[0]);dek.append(jokers[1])
+randomdek = list()
+for x in range(0,len(dek)):
+    randomnummer = random.randint(0,len(dek))
+    randomdek.append(dek[randomnummer-1])
+    dek.remove(dek[randomnummer-1])
+zevenKaarten = (randomdek[0],randomdek[1],randomdek[2],randomdek[3],randomdek[4],randomdek[5],randomdek[6])
+for lijstje in zevenKaarten:
+    print(lijstje)
+print()
+printKaarten= ''
+for x in range(7,54):
+    printKaarten += randomdek[x]+" "
+print(printKaarten)
