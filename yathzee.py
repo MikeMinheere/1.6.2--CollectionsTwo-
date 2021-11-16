c4d63d300f7a1e10d87866e6a1945f6a92c6d666
Mike Minheere <99067548@mydavinci.nl>
2021-11-16 14:05:24 +0100

    yathzeeeee

--- yathzee.py
+++ yathzee.py
@@ -1,6 +1,20 @@
 import random
 import time
 
+een=list()
+twee=list()
+drie=list()
+vier=list()
+vijf=list()
+zes=list()
+threeOfKind=list()
+fourOfKind=list()
+fullHouse=list()
+sStraight=list()
+lStraight=list()
+yathzee=list()
+chance=list()
+
 input('druk op enter om te starten >> ')
 print('u gaat nu Yathzee spelen')
 time.sleep(2)
@@ -43,33 +57,58 @@ print()
 time.sleep(1)
 print('dit zijn de cijfers die jij opzij gelegd hebt:')
 time.sleep(1)
-keuze = ['eenen','tweeën','drieën','vieren','vijfen','zessen','three of a kind','four of a kind','full house','small straight','large straight','yathzee','chance']
-
 print(*dobbelsOpzij, sep=", ")
-scoreOpslaan = input('je kan je score opslaan als: ' + str(keuze))
+keuze = ['eenen','tweeën','drieën','vieren','vijfen','zessen','three of a kind','four of a kind','full house','small straight','large straight','yathzee','chance']
+time.sleep(1)
+print()
+print('je kan je score opslaan als: ' + str(keuze)+ ' ')
+time.sleep(1)
+scoreOpslaan=input('type het woord in van de lijst van hoe je hem wilt opslaan>> ')
 if scoreOpslaan == 'eenen':
-    keuze.remove(0)
+    keuze.remove('eenen')
+    a=0
+    for i in range(len(dobbelsOpzij)):
+        if dobbelsOpzij[a] == 1:
+            een.append(dobbelsOpzij[a])
+        a+=1
 elif scoreOpslaan == 'tweeën' or scoreOpslaan == 'tweeen':
-    keuze.remove(1)
-elif scoreOpslaan == 'drieen' or scoreOpslaan == 'drieen':
-    keuze.remove(2)
+    keuze.remove('tweeën')
+    a=0
+    for i in range(len(dobbelsOpzij)):
+        if dobbelsOpzij[a] == 2:
+            twee.append(dobbelsOpzij[a])
+        a+=1
+elif scoreOpslaan == 'drieën' or scoreOpslaan == 'drieen':
+    keuze.remove('drieën')
+    drie.append(dobbelsOpzij)
 elif scoreOpslaan == 'vieren':
-    keuze.remove(3)
+    keuze.remove('vieren')
+    vier.append(dobbelsOpzij)
 elif scoreOpslaan == 'vijfen':
-    keuze.remove(4)
+    keuze.remove('vijfen')
+    vijf.append(dobbelsOpzij)
 elif scoreOpslaan == 'zessen':
-    keuze.remove(5)
+    keuze.remove('zessen')
+    zes.append(dobbelsOpzij)
 elif scoreOpslaan == 'three of a kind':
-    keuze.remove(6)
+    keuze.remove('three of a kind')
+    threeOfKind.append(dobbelsOpzij)
 elif scoreOpslaan == 'four of a kind':
-    keuze.remove(7)
+    keuze.remove('four of a kind')
+    fourOfKind.append(dobbelsOpzij)
 elif scoreOpslaan == 'full house':
-    keuze.remove(8)
+    keuze.remove('full house')
+    fullHouse.append(dobbelsOpzij)
 elif scoreOpslaan == 'small straight':
-    keuze.remove(9)
+    keuze.remove('small straight')
+    sStraight.append(dobbelsOpzij)
 elif scoreOpslaan == 'large straight':
-    keuze.remove(10)
+    keuze.remove('large straight')
+    lStraight.append(dobbelsOpzij)
 elif scoreOpslaan == 'yathzee':
-    keuze.remove(11)
+    keuze.remove('yathzee')
+    yathzee.append(dobbelsOpzij)
 elif scoreOpslaan == 'chance':
-    keuze.remove(12)
+    keuze.remove('chance')
+    chance.append(dobbelsOpzij)
+print(*een,sep=", ")
