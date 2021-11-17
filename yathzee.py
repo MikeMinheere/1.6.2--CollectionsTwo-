f7bbd001f7d9916a54a4321e4f046997aa4075c4
Mike Minheere <99067548@mydavinci.nl>
2021-11-17 12:50:05 +0100

    yathzeere

--- yathzee.py
+++ yathzee.py
@@ -1,5 +1,8 @@
 import random
 import time
+import webbrowser
+import collections
+from collections import Counter
 
 een=list()
 twee=list()
@@ -10,13 +13,16 @@ zes=list()
 threeOfKind=list()
 fourOfKind=list()
 fullHouse=list()
-sStraight=list()
 lStraight=list()
 yathzee=list()
 chance=list()
 
 input('druk op enter om te starten >> ')
 print('u gaat nu Yathzee spelen')
+help = input('wilt u de spelregels weten? (J/N)').upper()
+if help == 'J':
+    webbrowser.open("https://www.dobbelstenenshop.nl/spelregels-yahtzee/")
+else:()
 time.sleep(2)
 aantalDobbelstenen = 5
 dobbelen = list()
@@ -63,6 +69,7 @@ time.sleep(1)
 print()
 print('je kan je score opslaan als: ' + str(keuze)+ ' ')
 time.sleep(1)
+print()
 scoreOpslaan=input('type het woord in van de lijst van hoe je hem wilt opslaan>> ')
 if scoreOpslaan == 'eenen':
     keuze.remove('eenen')
@@ -71,6 +78,7 @@ if scoreOpslaan == 'eenen':
         if dobbelsOpzij[a] == 1:
             een.append(dobbelsOpzij[a])
         a+=1
+#dit voegt alle cijfers toe aan een aparte list om de score mee te berekenen
 elif scoreOpslaan == 'tweeën' or scoreOpslaan == 'tweeen':
     keuze.remove('tweeën')
     a=0
@@ -80,35 +88,74 @@ elif scoreOpslaan == 'tweeën' or scoreOpslaan == 'tweeen':
         a+=1
 elif scoreOpslaan == 'drieën' or scoreOpslaan == 'drieen':
     keuze.remove('drieën')
-    drie.append(dobbelsOpzij)
+    a=0
+    for i in range(len(dobbelsOpzij)):
+        if dobbelsOpzij[a] == 3:
+            drie.append(dobbelsOpzij[a])
+        a+=1
 elif scoreOpslaan == 'vieren':
     keuze.remove('vieren')
-    vier.append(dobbelsOpzij)
+    a=0
+    for i in range(len(dobbelsOpzij)):
+        if dobbelsOpzij[a] == 4:
+            vier.append(dobbelsOpzij[a])
+        a+=1
 elif scoreOpslaan == 'vijfen':
     keuze.remove('vijfen')
-    vijf.append(dobbelsOpzij)
+    a=0
+    for i in range(len(dobbelsOpzij)):
+        if dobbelsOpzij[a] == 5:
+            vijf.append(dobbelsOpzij[a])
+        a+=1
 elif scoreOpslaan == 'zessen':
     keuze.remove('zessen')
-    zes.append(dobbelsOpzij)
+    a=0
+    for i in range(len(dobbelsOpzij)):
+        if dobbelsOpzij[a] == 6:
+            zes.append(dobbelsOpzij[a])
+        a+=1
 elif scoreOpslaan == 'three of a kind':
     keuze.remove('three of a kind')
-    threeOfKind.append(dobbelsOpzij)
+    dup = {x for x in dobbelsOpzij if dobbelsOpzij.count(x) == 3}
+    if len(dup) > 0:
+        threeOfKind.append(dobbelsOpzij)
 elif scoreOpslaan == 'four of a kind':
     keuze.remove('four of a kind')
-    fourOfKind.append(dobbelsOpzij)
+    dup = {x for x in dobbelsOpzij if dobbelsOpzij.count(x) == 4}
+    if len(dup) > 0:
+        fourOfKind.append(dobbelsOpzij)
 elif scoreOpslaan == 'full house':
     keuze.remove('full house')
-    fullHouse.append(dobbelsOpzij)
+    dup = {x for x in dobbelsOpzij if dobbelsOpzij.count(x) == 3}
+    dup2 = {x for x in dobbelsOpzij if dobbelsOpzij.count(x) == 2}
+    if len(dup) > 0 and len(dup2) > 0:
+        fullHouse.append(dobbelsOpzij)
+    print(fullHouse)
 elif scoreOpslaan == 'small straight':
     keuze.remove('small straight')
-    sStraight.append(dobbelsOpzij)
+    sStraight = False
+    y = [0,0,0]
+    for x in range(1,7):
+        y[0] = dobbelsOpzij.count(x)
+        y[1] = dobbelsOpzij.count(x+1)
+        y[2] = dobbelsOpzij.count(x+2)
+        if y[0] > 0 and y[1] > 0 and y[2] > 0:
+            sStraight = True
 elif scoreOpslaan == 'large straight':
     keuze.remove('large straight')
-    lStraight.append(dobbelsOpzij)
+    lStraight = False
+    y = [0,0,0,0]
+    for x in range(1,7):
+        y[0] = dobbelsOpzij.count(x)
+        y[1] = dobbelsOpzij.count(x+1)
+        y[2] = dobbelsOpzij.count(x+2)
+        y[3] = dobbelsOpzij.count(x+3)
+        if y[0] > 0 and y[1] > 0 and y[2] > 0:
+            lStraight = True
 elif scoreOpslaan == 'yathzee':
     keuze.remove('yathzee')
     yathzee.append(dobbelsOpzij)
 elif scoreOpslaan == 'chance':
     keuze.remove('chance')
     chance.append(dobbelsOpzij)
-print(*een,sep=", ")
+    
