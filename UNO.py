9481a64fdce17eb9ec323d4c5e3ac36f9f4c2a74
Mike Minheere <99067548@mydavinci.nl>
2021-12-07 14:35:07 +0100

    unonono

--- UNO.py
+++ UNO.py
@@ -3,13 +3,13 @@ seed = random.randint(1,1000000)
 random.seed(seed)
 print(f"seed: {seed}")
 dek=list()
-kleuren=['groen ','blauw ','geel ','rood ']
+kleuren=['0','1','2','3']
 cijfer=['1','2','3','4','5','6','7','8','9']
-neemTwee='+2 kaart'
-beurtOverslaan='beurt overslaan kaart'
-volgorde='keer-om kaart'
-neemVier='+4 kaart'
-kiesKleur='kies kaart'
+neemTwee='10'
+beurtOverslaan='11'
+volgorde='12'
+neemVier='4.1'
+kiesKleur='4.0'
 spelers=list()
 
 i = True
@@ -23,27 +23,28 @@ while i==True:
         print('je hebt minimaal 2 spelers nodig om te spelen')
 for x in range(2):
     for i in range(len(kleuren)):
-        dek.append(kleuren[i]+neemTwee)
+        dek.append(kleuren[i]+"."+neemTwee)
 for x in range(2):
     for i in range(len(kleuren)):
-        dek.append(kleuren[i]+beurtOverslaan)
+        dek.append(kleuren[i]+"."+beurtOverslaan)
 for x in range(2):
     for i in range(len(kleuren)):
-        dek.append(kleuren[i]+volgorde)
-for x in range(2):
-    for i in range(len(kleuren)):
-        dek.append(kleuren[i]+kiesKleur)
+        dek.append(kleuren[i]+"."+volgorde)
+for x in range(4):
+    dek.append(kiesKleur)
+for x in range(4):
+    dek.append(neemVier)
 for i in range(2):
     for x in range(len(cijfer)):
-        dek.append(kleuren[0]+cijfer[x])
+        dek.append(kleuren[0]+"."+cijfer[x])
     for x in range(len(cijfer)):
-        dek.append(kleuren[1]+cijfer[x])
+        dek.append(kleuren[1]+"."+cijfer[x])
     for x in range(len(cijfer)):
-        dek.append(kleuren[2]+cijfer[x])
+        dek.append(kleuren[2]+"."+cijfer[x])
     for x in range(len(cijfer)):
-        dek.append(kleuren[3]+cijfer[x])
+        dek.append(kleuren[3]+"."+cijfer[x])
 for i in range(len(kleuren)):
-    dek.append(kleuren[i]+' 0')
+    dek.append(kleuren[i]+'.0')
 random.shuffle(dek)
 for x in range(aantalSpelers):
     spelers.append(list())
@@ -56,3 +57,6 @@ for i in range(aantalSpelers):
 print(len(dek))
 eersteKaart=dek[0]
 print(eersteKaart)
+print(dek)
+print(eersteKaart.split("."))
+print(spelers[1])
