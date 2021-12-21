363cee5cb078e1be0d1ceadc547b7d56e3a1fad0
Mike Minheere <99067548@mydavinci.nl>
2021-12-21 09:56:37 +0100

    uno

--- UNO.py
+++ UNO.py
@@ -3,13 +3,13 @@ seed = random.randint(1,1000000)
 random.seed(seed)
 print(f"seed: {seed}")
 dek=list()
-kleuren=['0','1','2','3']
+kleuren=['groen','blauw','geel','rood']
 cijfer=['1','2','3','4','5','6','7','8','9']
-neemTwee='10'
-beurtOverslaan='11'
-volgorde='12'
-neemVier='4.1'
-kiesKleur='4.0'
+neemTwee=' +2 kaart'
+beurtOverslaan='beurt overslaan kaart'
+volgorde='keer-om kaart'
+neemVier='+4 kaart'
+kiesKleur='kies kaart'
 spelers=list()
 stapel=list()
 
@@ -22,6 +22,7 @@ while i==True:
         i = False
     elif aantalSpelers<=1:
         print('je hebt minimaal 2 spelers nodig om te spelen')
+        i = False
 for x in range(2):
     for i in range(len(kleuren)):
         dek.append(kleuren[i]+"."+neemTwee)
@@ -31,6 +32,9 @@ for x in range(2):
 for x in range(2):
     for i in range(len(kleuren)):
         dek.append(kleuren[i]+"."+volgorde)
+for x in range(2):
+    for i in range(len(kleuren)):
+        dek.append(kleuren[i]+"."+kiesKleur)
 for x in range(4):
     dek.append(kiesKleur)
 for x in range(4):
@@ -40,7 +44,6 @@ for i in range(2):
         dek.append(kleuren[0]+"."+cijfer[x])
     for x in range(len(cijfer)):
         dek.append(kleuren[1]+"."+cijfer[x])
-
     for x in range(len(cijfer)):
         dek.append(kleuren[2]+"."+cijfer[x])
     for x in range(len(cijfer)):
@@ -54,15 +57,7 @@ for i in range(aantalSpelers):
     for x in range(7):
         spelers[i].append(dek[x])
         del dek[x]
+print(dek)
 stapel=dek[0]
 stapel=stapel.split(".")
-print(stapel)
-for x in range(aantalSpelers):
-    for i in range(7):
-        spelers[x]+=(spelers[x][i].split("."))
-print(spelers)
-for i in range(7):
-    if stapel[0]== spelers[0][i]:
-        print('test')
-    elif stapel[1]== spelers[0][i]:
-        print('test')
+
